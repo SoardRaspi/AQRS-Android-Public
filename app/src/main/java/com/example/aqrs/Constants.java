391f6d49d2a4c6b7137661dc2e8c8ddfdd53baba
SoardRaspi <69724218+SoardRaspi@users.noreply.github.com>
2022-11-06 18:29:54 +0530

    Update Constants.java

--- app/src/main/java/com/example/aqrs/Constants.java
+++ app/src/main/java/com/example/aqrs/Constants.java
@@ -1,7 +1,8 @@
 package com.example.aqrs;
 
 public class Constants {
-    public static String USER_ID = "sec-c-MTJiY2E3OWUtOWQ2NC00Mjg5LWE0YTgtODE0ODM2NGI4NmE1";
-    public static String PUBLISH_KEY = "pub-c-0d90fa20-99d3-483a-8b06-2c2102e35f97";
-    public static String SUBSCRIBE_KEY = "sub-c-ef9c4357-01ec-4854-b8d3-4066fabf9f56";
+//     The following credentials are for Mapbox Maps API, which are not being used anymore
+//     public static String USER_ID = "sec-c-MTJiY2E3OWUtOWQ2NC00Mjg5LWE0YTgtODE0ODM2NGI4NmE1";
+//     public static String PUBLISH_KEY = "pub-c-0d90fa20-99d3-483a-8b06-2c2102e35f97";
+//     public static String SUBSCRIBE_KEY = "sub-c-ef9c4357-01ec-4854-b8d3-4066fabf9f56";
 }
